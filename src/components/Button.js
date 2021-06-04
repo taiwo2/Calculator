e46c96e1482ad5132f6d194b32b482d66aa11961
taiwo2 <adetonat@ymail.com>
2021-06-04 21:15:43 +0100

    Add ld default pr

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-const {
-  name, color, half, clickHandler,
-} = props;
-const styling = {
-  width: half ? '50%' : '25%',
-  background: color,
-};
 
-const Button = ({ name, HandleClick }) => (
-  <div>
-    <button onClick={() => HandleClick} 
-    className="buttons"
-    style={styling} 
-    type="button">
-      { name }
-    </button>
-  </div>
-);
+const Button = (props) => {
+  const {
+    name, color, half, HandleClick,
+  } = props;
+  const styling = {
+    width: half ? '50%' : '25%',
+    background: color,
+  };
+  return (
+    <div>
+      <button
+        onClick={() => HandleClick}
+        className="buttons"
+        style={styling}
+        type="button"
+      >
+        { name }
+      </button>
+    </div>
+  );
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
